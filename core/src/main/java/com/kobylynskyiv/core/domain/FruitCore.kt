ab80fd6b264c5c171c08d3e2a3dad3edf7460c8d
Vadym <vadym.kobylynskyi@ingenico.com>
2023-12-31 15:19:52 +0300

    update core pojo class

--- core/src/main/java/com/kobylynskyiv/core/domain/FruitCore.kt
+++ core/src/main/java/com/kobylynskyiv/core/domain/FruitCore.kt
@@ -1,7 +1,15 @@
 package com.kobylynskyiv.core.domain
 
+import androidx.annotation.Keep
+
+data class ResponseFruitCore(
+    val title: String,
+    val items: List<FruitCore>
+)
+
+
 data class FruitCore(
-    val id: Int = 0,
+    val id: String,
     val name: String,
     val image: String,
     val color: String
